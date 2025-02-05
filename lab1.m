3fb9f9267c09ad5f901b9cb9cf9bb813b29a9508
Jonatan Eklöv <jonatan.eklov@gmail.com>
2025-02-05 17:41:45 +0100

    Finilized and sent in lab1

--- lab1.m
+++ lab1.m
@@ -6,16 +6,13 @@ clf
 
 x = [2,3,4,5,6];
 y = [2,6,5,5,6];
-%figure(),
 plot(x,y,'r*', 'MarkerSize',16)
-
-
-c = polyfit(x,y,4)
-
-xx = linspace(min(x),max(x),50);
-yy = polyval(c,xx)
-
 hold on
+
+c = polyfit(x,y,4);
+xx = linspace(min(x),max(x),50);
+yy = polyval(c,xx);
+
 plot(xx,yy, 'k')
 legend("Data points", "4 deg interpolant");
 saveas(gcf,'lab1.task1.png')
@@ -26,13 +23,14 @@ clf
 x = [1975,1980,1985,1990];
 y_west = [72.8, 74.2, 75.2, 76.4];
 y_east = [70.2, 70.2, 70.3, 71.2];
-
 plot(x,y_west,'b*', x,y_east,'r*')
 hold on
+
+% Constructing interpolation
 c_west = polyfit(x,y_west,length(x)-1);
 c_east = polyfit(x,y_east,length(x)-1);
 
-% Interpolant
+% Plotting interpolation
 xx = linspace(1965,1995,50);
 yy_west = polyval(c_west,xx);
 yy_east = polyval(c_east,xx);
@@ -48,19 +46,22 @@ plot(1970, y_west_70, 'bo', 1970, y_east_70, 'ro')
 plot(1970, 71.8, "bpentagram")
 plot(1970, 69.6, "rpentagram")
 
+% Finializing plot and printing
 legend("Data west", "Data east", "Interp. west", "Interp. east")
 xlim([1965 1995])
 saveas(gcf,'lab1.task2.png')
 
 %% Task 3
 
+% Data for testing 
 x = [1975,1980,1985,1990];
 y_west = [72.8, 74.2, 75.2, 76.4];
 y_east = [70.2, 70.2, 70.3, 71.2];
 
-
+% Testing function
 y_west_70 = MyLagrangeInterpol(x,y_west,1970)
 y_easy_70 = MyLagrangeInterpol(x,y_east,1970)
+
 
 function yy = MyLagrangeInterpol(x,y,xx)
     n = length(x);
